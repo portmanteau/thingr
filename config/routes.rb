cbf02759fc32e52c0d7d6451df4336f3cd64b5fa
Lee Pender <Lpender@gmail.com>
2014-10-17 14:49:46 -0400

    WIP User login with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'index/index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  root to: "high_voltage/pages#show", id: "home"
   resources :items
 end
