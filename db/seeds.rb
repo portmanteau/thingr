0c9bf6fa8f7b0cdd67f44a9bd033fa39f8f88451
Zander Mackie <zmackie@gmail.com>
2015-04-06 16:52:27 -0400

    Added item image spec and views.  created db seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(email: "zander@example.com", password: "password1234")
+
+10.times do |i|
+  Item.create(name: "Item #{i}", description: "Better than item # #{i-1}", 
+              image: File.open("spec/asset_specs/photos/item.JPG"), user: User.first) 
+end
--- spec/features/user_adds_a_new_item_spec.rb
+++ spec/features/user_adds_a_new_item_spec.rb
@@ -9,8 +9,10 @@ feature "User Adds A New Item" do
     fill_in t("simple_form.labels.item.name"), with: "Rad Item"
     fill_in t("simple_form.labels.item.description"),
       with: "The best thing ever.\n Better than everything"
+    attach_file('Image', 'spec/asset_specs/photos/item.jpg')
 
     click_button t("helpers.submit.item.create")
     expect(page).to have_content("Rad Item")
+    expect(page).to have_selector("img[src*='item.jpg']")
   end
 end
