d2629162c9a552795c116ab6264a285b54359d9f
Zander Mackie <zmackie@gmail.com>
2015-04-07 13:57:44 -0400

    Cleaned up junk specs

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -3,17 +3,9 @@ require 'spec_helper'
 describe IndexController do
 
   describe "GET 'action'" do
-    it "returns http success" do
-      get 'action'
-      response.should be_success
-    end
   end
 
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
   end
 
 end
